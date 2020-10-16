3fa2a6a815cef34296ed904300c29b183046b4c9
Joao Inez <joaoinez.info@gmail.com>
2020-10-16 18:37:12 +0100

    Firebase config now uses environment variables

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,10 +1,10 @@
 export default {
-  apiKey: "AIzaSyA54fI6o7Om0aCDbbTYR-Sm9tv9D2NIDFY",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "haikus-pra-vida.firebaseapp.com",
   databaseURL: "https://haikus-pra-vida.firebaseio.com",
   projectId: "haikus-pra-vida",
   storageBucket: "haikus-pra-vida.appspot.com",
-  messagingSenderId: "206335038788",
-  appId: "1:206335038788:web:0c2670dc2b5389bfeb3311",
-  measurementId: "G-2Y8M4W276F",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
